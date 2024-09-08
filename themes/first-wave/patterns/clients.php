1200a8c804debf4cb3fee9ab54de6be7a1c9e8a8
Danilo Parra Jr. <daniloparrajr.work@gmail.com>
2024-09-08 11:56:15 +0800

    feat: add header and footer templates

---
+++ themes/first-wave/patterns/clients.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Title: Clients
+ * Slug: first-wave/clients
+ * Inserter: true
+ */
+?>
+
+<!-- wp:group {"metadata":{"name":"Clients"},"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|xxx-large","bottom":"var:preset|spacing|xxx-large","left":"var:preset|spacing|x-large","right":"var:preset|spacing|x-large"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"border":{"radius":"40px"}},"backgroundColor":"contrast","textColor":"base","layout":{"type":"constrained","justifyContent":"center"}} -->
+<div class="wp-block-group alignfull has-base-color has-contrast-background-color has-text-color has-background has-link-color" style="border-radius:40px;padding-top:var(--wp--preset--spacing--xxx-large);padding-right:var(--wp--preset--spacing--x-large);padding-bottom:var(--wp--preset--spacing--xxx-large);padding-left:var(--wp--preset--spacing--x-large)"><!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
+	<div class="wp-block-group alignwide"><!-- wp:heading {"className":"is-style-heading-line","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|large"}},"typography":{"letterSpacing":"0.05em"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontSize":"base"} -->
+		<h2 class="wp-block-heading is-style-heading-line has-base-color has-text-color has-link-color has-base-font-size" style="margin-bottom:var(--wp--preset--spacing--large);letter-spacing:0.05em">I’ve worked with hundreds of amazing people</h2>
+		<!-- /wp:heading -->
+
+		<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|large"}},"layout":{"type":"grid","minimumColumnWidth":"13rem"}} -->
+		<div class="wp-block-group"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+			<figure class="wp-block-image size-full"><img src="http://daniloparrajr.local/wp-content/uploads/2024/08/logo-light.ada2d552.png" alt=""/></figure>
+			<!-- /wp:image -->
+
+			<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+			<figure class="wp-block-image size-full"><img src="http://daniloparrajr.local/wp-content/uploads/2024/08/logo-light.5c8de99c.png" alt=""/></figure>
+			<!-- /wp:image -->
+
+			<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+			<figure class="wp-block-image size-full"><img src="http://daniloparrajr.local/wp-content/uploads/2024/08/logo-light.f6ce2960.png" alt=""/></figure>
+			<!-- /wp:image -->
+
+			<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+			<figure class="wp-block-image size-full"><img src="http://daniloparrajr.local/wp-content/uploads/2024/08/logo-light.249606f6.png" alt=""/></figure>
+			<!-- /wp:image -->
+
+			<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+			<figure class="wp-block-image size-full"><img src="http://daniloparrajr.local/wp-content/uploads/2024/08/logo-light.ada2d552.png" alt=""/></figure>
+			<!-- /wp:image -->
+
+			<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+			<figure class="wp-block-image size-full"><img src="http://daniloparrajr.local/wp-content/uploads/2024/08/logo-light.5c8de99c.png" alt=""/></figure>
+			<!-- /wp:image -->
+
+			<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+			<figure class="wp-block-image size-full"><img src="http://daniloparrajr.local/wp-content/uploads/2024/08/logo-light.f6ce2960.png" alt=""/></figure>
+			<!-- /wp:image -->
+
+			<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+			<figure class="wp-block-image size-full"><img src="http://daniloparrajr.local/wp-content/uploads/2024/08/logo-light.249606f6.png" alt=""/></figure>
+			<!-- /wp:image --></div>
+		<!-- /wp:group --></div>
+	<!-- /wp:group --></div>
+<!-- /wp:group -->
